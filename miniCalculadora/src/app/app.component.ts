05f6abdb375de2a624ea83a5c288d67005b6c355
Edwin Calle <calleojeda4@gmail.com>
2023-10-14 15:15:57 -0500

    fin app interpolacion, property biding , event binding, binding bidirecciona

--- miniCalculadora/src/app/app.component.ts
+++ miniCalculadora/src/app/app.component.ts
@@ -14,4 +14,13 @@ export class AppComponent {
   sumar(): void {
     this.resultado = this.numero1 + this.numero2;
   }
+  restar(): void {
+    this.resultado = this.numero1 - this.numero2;
+  }
+  multiplicacion(): void {
+    this.resultado = this.numero1 * this.numero2;
+  }
+  divicion(): void {
+    this.resultado = this.numero1 / this.numero2;
+  }
 }
